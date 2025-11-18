1876dff45c32a3d5ec983f8652701f647f495d1e
alfrix <140119318+oalfrix@users.noreply.github.com>
2025-11-18 15:32:09 +0300

    upt

--- main.js
+++ main.js
@@ -12,4 +12,15 @@ console.log(name);
     console.log(typeof age); // "number"
     console.log(typeof isStudent); // "boolean"
     console.log(typeof address); // "object"
-    console.log(typeof phoneNumber); // "undefined" --- IGNORE ---
+    console.log(typeof phoneNumber); // "undefined"
+
+
+// Concatenation
+    let fullName = name + " Smith";
+    console.log(fullName); // "Alice Smith"
+
+// Template Literals
+    let greeting = `Hello, my name is ${name} and I am ${age} years old.`;
+    console.log(greeting); // "Hello, my name is Alice and I am 30 years old."
+
+    //string properties and methods 
